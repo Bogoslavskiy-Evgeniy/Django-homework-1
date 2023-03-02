67881fd25363a37b164f6e308e016a48bbc76975
Bogoslavskiy-Evgeniy <bogoslavskiy.ev@gmail.com>
2023-03-02 23:34:06 +0300

    Homework 2

--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 
 from django.urls import path
 
+from calculator.views import recipe_list
+
 urlpatterns = [
-    # здесь зарегистрируйте вашу view-функцию
+    path('<dish>/', recipe_list),
+
 ]
