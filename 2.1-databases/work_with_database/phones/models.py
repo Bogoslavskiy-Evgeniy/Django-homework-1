e3732ee2ecd84f18a8e29c62ba9b5c352b84a044
Bogoslavskiy-Evgeniy <bogoslavskiy.ev@gmail.com>
2023-03-20 20:46:24 +0300

    Homework ORM 1.1

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,22 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 class Phone(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    image = models.ImageField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+    def save(self):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save()
+
     # TODO: Добавьте требуемые поля
-    pass
+    # pass
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +9,30 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    phone_objects = Phone.objects.all()
+    page_sort = request.GET.get('sort', '')
+
+    if page_sort == 'max_price':
+        phones = phone_objects.order_by('-price')
+        context = {'phones': phones}
+        return render(request, template, context)
+
+    elif page_sort == 'min_price':
+        phones = phone_objects.order_by('price')
+        context = {'phones': phones}
+        return render(request, template, context)
+
+    elif page_sort == 'name':
+        phones = phone_objects.order_by('name')
+        context = {'phones': phones}
+        return render(request, template, context)
+
+    context = {'phones': phone_objects}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone_object = Phone.objects.get(slug=slug)
+    context = {'phone': phone_object}
     return render(request, template, context)
