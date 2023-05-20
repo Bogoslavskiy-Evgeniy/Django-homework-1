2baff78cae62d44cfd2002ddaac5ad20bf6483ca
Bogoslavskiy-Evgeniy <bogoslavskiy.ev@gmail.com>
2023-05-20 14:32:14 +0300

    add check

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,5 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import api_view
 from rest_framework.filters import SearchFilter
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
@@ -22,3 +24,6 @@ class StockViewSet(ModelViewSet):
     filterset_fields = ['products']
     search_field = ['products']
 
+@api_view(['GET'])
+def sample_view(request):
+    return Response('This is checking')
